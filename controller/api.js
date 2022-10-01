f2d964daea646694735d34d13da90b28f17c8fb5
Dennis Meyer <dennism3991@googlemail.com>
2022-10-01 14:37:30 +0200

    Add PATCH and DELETE

--- controller/api.js
+++ controller/api.js
@@ -23,7 +23,6 @@ module.exports = class API {
   //create post
   static async createPost(req, res) {
     const post = req.body;
-
     try {
       await Post.create(post);
       res.status(201).json({ message: "Post created" });
@@ -32,11 +31,24 @@ module.exports = class API {
     }
   }
   // update post
-  static async updatePost(reg, res) {
-    res.send("update post");
+  static async updatePost(req, res) {
+    const id = req.params.id;
+    const newPost = req.body;
+    try {
+      await Post.findByIdAndUpdate(id, newPost);
+      res.status(200).json({ message: "Post updated" });
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
   }
   //delete a post
-  static async deletePost(reg, res) {
-    res.send("delete post");
+  static async deletePost(req, res) {
+    const id = req.params.id;
+    try {
+      const result = await Post.findByIdAndDelete(id);
+      res.status(200).json({ message: "Post deleted" });
+    } catch (error) {
+      res.status(404).json({ message: err.message });
+    }
   }
 };
